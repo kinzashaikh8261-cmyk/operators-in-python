b4141a46dd41f465655468ad362bda87c247302d
Kinza_aznik <kinzashaikh8261@gmail.com>
2026-06-25 17:01:22 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,43 @@
+#arithematic operators
+a = 3
+b = 2
+sum = a + b
+diff = a - b
+multi = a * b
+div = a / b
+modulus = a % b
+power = a ** b 
+print(sum)
+print(diff)
+print(multi)
+print(div)
+print(modulus)
+print(power)
+
+#realtional operators
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#assignment operator
+n = 4
+n += 10
+n -= 10
+n *= 10
+n /= 10
+n %= 10
+n **= 10
+
+#logical operators 
+a=30
+b=20
+print(not False)
+print(not (a<b))
+
+num1 = True
+num2 = False
+print("AND operator : ", num1 and num2 )
+print("OR operator:", (a>b) or (a==b))
